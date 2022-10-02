7969bd4d66f8553546228f8e1d22f659df9a35b3
Dark <giovanne13131@gmail.com>
2022-10-02 14:10:52 -0300

    Project Setup

---
+++ src/main/java/io/github/darkgr/monolith/Monolith.java
@@ -0,0 +1,15 @@
+package io.github.darkgr.monolith;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Monolith.MOD_ID)
+public class Monolith {
+    public static final String MOD_ID = "monolith";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Monolith() {
+
+    }
+}
